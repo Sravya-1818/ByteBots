9798f2967c332733bd75065d4f86f3a8a09e6caa
Sravya-1818 <sravyalakshmitulasivalluri@gmail.com>
2025-06-14 09:00:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import QRGenerator from "./pages/QRGenerator";
-import UserProfile from "./pages/UserProfile";
-import NotFound from "./pages/NotFound";
+import Index from "../pages/Index";
+import QRGenerator from "../pages/QRGenerator";
+import UserProfile from "../pages/UserProfile";
+import NotFound from "../pages/NotFound";
 import Login from "../pages/Login";
 
 
